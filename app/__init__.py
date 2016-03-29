329e548281b5679b3c6cf786f3ee58e7664772d9
garryforgit <a45120466@gmail.com>
2016-03-29 20:38:31 +0800

    version 0.5 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 #coding=utf-8
 #!/home/purplemaple/py2/microblog/flaskt/bin/python
+import os
 from flask import Flask
+from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 from app import views, models
 track_modifications = app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
 app.config['SQLALCHEMY_TRACK_MODFICATIONS'] = True
+
