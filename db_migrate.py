bc359ac6a9cbb17f7d454fafafeeb8ddbecb6f3c
garryforgit <a45120466@gmail.com>
2016-03-31 17:58:00 +0800

    versiong0.6

--- db_migrate.py
+++ db_migrate.py
@@ -14,7 +14,7 @@ exec old_model in tmp_module.__dict__
 script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI ,SQLALCHEMY_MIGRATE_REPO, tmp_module.meta, db.metadata)
 open(migration,"wt").write(script)
 api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-vs = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 print ('New migraton saved as ' + migration)
-print ('Current database version: ' + str(vs))
+print ('Current database version: ' + str(v))
 
