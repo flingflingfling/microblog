d77873eafb92833ad0bcc250fd1792b196b0ba99
garryforgit <a45120466@gmail.com>
2016-04-01 16:20:18 +0800

    add some unittest & function

--- tests.py
+++ tests.py
@@ -5,7 +5,9 @@ import unittest
 
 from config import basedir
 from app import app, db
-from app.models import User
+from app.models import User, Post
+from datetime import datetime, timedelta
+
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -37,6 +39,85 @@ class TestCase(unittest.TestCase):
         nickname2 = User.make_unique_nickname('john')
         assert nickname2 != 'john'
         assert nickname2 != nickname
+
+    def test_follow(self):
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='sus', email='sus@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.unfollow(u2) is None
+        u = u1.follow(u2)
+        db.session.add(u)
+        db.session.commit()
+        assert u1.follow(u2) is None
+        assert u1.is_following(u2)
+        assert u1.followed.count() == 1
+        assert u1.followed.first().nickname == 'sus'
+        assert u2.followers.count() ==1
+        assert u2.followers.first().nickname == 'john'
+        u = u1.unfollow(u2)
+        assert u is not None
+        db.session.add(u)
+        db.session.commit()
+        assert not u1.is_following(u2)
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
+    def test_follow_posts(self):
+        #make four users
+        u1 = User(nickname='jo', email='jo@example.com')
+        u2 = User(nickname='su', email='su@example.com')
+        u3 = User(nickname='ma', email='ma@example.com')
+        u4 = User(nickname='da', email='da@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(u3)
+        db.session.add(u4)
+        #make 4 posts
+        utcnow = datetime.utcnow()
+        p1 = Post(body="post from jo", author = u1,timestamp=utcnow + timedelta(seconds=1))
+        p2 = Post(body="post from su", author = u2,timestamp=utcnow + timedelta(seconds=2))
+        p3 = Post(body="post from ma", author = u3,timestamp=utcnow + timedelta(seconds=3))
+        p4 = Post(body="post from da", author = u4,timestamp=utcnow + timedelta(seconds=4))
+        db.session.add(p1)
+        db.session.add(p2)
+        db.session.add(p3)
+        db.session.add(p4)
+        db.session.commit()
+        #set up the followers
+        u1.follow(u1)
+        u1.follow(u2)
+        u1.follow(u4)
+        u2.follow(u2)
+        u2.follow(u3)
+        u3.follow(u3)
+        u3.follow(u4)
+        u4.follow(u4)
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(u3)
+        db.session.add(u4)
+        db.session.commit()
+        #check th followed posts of each user
+        f1 = u1.followed_posts().all()
+        f2 = u2.followed_posts().all()
+        f3 = u3.followed_posts().all()
+        f4 = u4.followed_posts().all()
+        assert len(f1) == 3
+        assert len(f2) == 2
+        assert len(f3) == 2
+        assert len(f4) == 1
+        assert f1 == [p4, p2, p1]
+        assert f2 == [p3, p2]
+        assert f3 == [p4, p3]
+        assert f4 == [p4]
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
